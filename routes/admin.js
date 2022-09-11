a835e26f11a5e788e7785a2f844e781b31a4e719
Douglas Felchilcher <douglas@onlinebr.net>
2022-09-11 15:02:57 -0300

    Categoria: model e views

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
 
 
 router.get('/', (requisicao, resposta) => {
-  //resposta.send("Página principal do admin")
   resposta.render("admin/index")
 })
 
@@ -12,7 +14,24 @@ router.get('/posts', (requisicao, resposta) => {
 })
 
 router.get('/categorias', (requisicao, resposta) => {
-  resposta.send("Página de categorias")
+  resposta.render("admin/categorias")
+})
+
+router.get('/categorias/adicionar', (requisicao, resposta) => {
+  resposta.render("admin/categorias_adicionar")
+})
+
+router.post('/categorias/nova', (requisicao, resposta) => {
+  const novaCategoria = {
+    nome: requisicao.body.nome,
+    slug: requisicao.body.slug
+  }
+
+  new Categoria(novaCategoria).save().then(() => {
+    console.log("Categoria salva com sucesso")
+  }).catch((erro) => {
+    console.log("Erro ao salvar a categoria "+erro)
+  })
 })
 
 module.exports = router
